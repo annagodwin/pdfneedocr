1737179f7dadaf20e82afbc177466de6e01cf757
Anna Godwin <anna.godwin17@gmail.com>
2022-12-12 17:03:46 -0500

    Update test files

--- tests/test_check.py
+++ tests/test_check.py
@@ -1,15 +1,15 @@
 from pdfneedocr.check import pdf_need_ocr_check
 
 
-def test_pdf_need_ocr_check_needs_ocr():
+def test_pdf_need_ocr_check_need_ocr_false():
 
-    outcome = pdf_need_ocr_check("tests/data/82252956_2958.pdf")
-
-    assert outcome is True
-
-
-def test_pdf_need_ocr_check_machine_readable():
-
-    outcome = pdf_need_ocr_check("tests/data/82252956_2958_ocr.pdf")
+    outcome = pdf_need_ocr_check("tests/data/text_need_ocr_false.pdf")
 
     assert outcome is False
+
+
+def test_pdf_need_ocr_check_need_ocr_true():
+
+    outcome = pdf_need_ocr_check("tests/data/text_need_ocr_true.pdf")
+
+    assert outcome is True
